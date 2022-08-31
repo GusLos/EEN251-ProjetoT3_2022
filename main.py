a4191d6f6f86d2f81f4bebe29cd4c8de6722c569
GusLos <gu.losada@hotmail.com>
2022-08-31 08:54:24 -0300

    Inicio do projeto para criar um assistente virtual.

---
+++ main.py
@@ -0,0 +1,27 @@
+from threading import Timer
+import sys
+
+def chamar():
+    # t = Timer(5, chamar)
+    # t.start()
+    # texto = input('>')
+    # return texto
+    pass
+
+    pass
+
+def validar(x: str):
+    if (x == 'python'):
+        print('Ola.')
+
+
+if __name__ == '__main__':
+
+    op = ''
+    if op == 'python':
+        print('Ola')
+    # while True:
+    #     string = console()
+    #     if string == 'quit':
+    #         break
+    #     pass
