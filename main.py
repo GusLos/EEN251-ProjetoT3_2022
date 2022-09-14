2927dfd7cb9c91b194e326fbb9b93dedfa08f2d7
GusLos <gu.losada@hotmail.com>
2022-09-14 13:59:39 -0300

    Começo da classe para tocar música
    Usando o youtube no navegador

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from threading import Timer
-import sys
-
-def chamar():
-    # t = Timer(5, chamar)
-    # t.start()
-    # texto = input('>')
-    # return texto
-    pass
-
-    pass
 
 def validar(x: str):
     if (x == 'python'):
