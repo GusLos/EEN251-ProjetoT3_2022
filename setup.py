407fe4aeef950d285487fbf6fb626110a5100537
GUSTAVO LOURENCO LOSADA <19.00345-5@maua.br>
2022-10-04 16:07:57 -0300

    Ajustando arquivo setup

--- setup.py
+++ setup.py
@@ -6,23 +6,27 @@ except ImportError:
     from pip._internal import main as pipmain
 
 
-pipmain(['install', 'nltk'])
-pipmain(['install','pathlib '])
+# pipmain(['install', 'nltk'])
+# pipmain(['install','pathlib '])
 
-import pathlib
-import nltk
-path_nltk = pathlib.PurePath('.','nltk')
+# import pathlib
+# import nltk
+# path_nltk = pathlib.PurePath('.','nltk')
 
 attempts: int = 0
 while attempts <3:
     try:
-        nltk.data.path.append(path_nltk)
+        # nltk.data.path.append(path_nltk)
+        pipmain(['install','selenium'])
+        pipmain(['install','SpeechRecognition'])
+        pipmain(['install','pyttsx3'])
+        pipmain(['install','PyAudio'])
         attempts = 10
     except Exception as  e:
         print(f'Error: {e}')
         print(f'Attempt {attempts + 1} failed, trying again.')
         attempts+=1
 
-nltk.download('stopwords', download_dir=path_nltk)
+# nltk.download('stopwords', download_dir=path_nltk)
 
 
